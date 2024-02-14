10bff2b8a875066767465a4f3f087370abc81de8
hammadsadi <hammad.sadi@yahoo.com>
2024-02-14 22:20:52 +0600

    Keyboard Dainamically Button vali get Success

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+// Continue The Game
+function continuegame() {
+  let alphabet = generateRandomAlphabet();
+  let showAlphabet = (document.getElementById("showAlphabet").innerText =
+    alphabet);
+  heigLightKeyById(alphabet);
+}
+
+// Get Key Press Button
+document.addEventListener("keyup", function () {
+  console.log("key press");
+});
+
 // Game Start
 function gameStart() {
   hideElementById("home");
   showElementById("play");
+  continuegame();
 }
--- js/utility.js
+++ js/utility.js
@@ -8,3 +8,19 @@ function showElementById(elementId) {
   let element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
+
+// Generate Random Alphabet
+function generateRandomAlphabet() {
+  let alphabetString = "abcdefghjklmnopqrstuvwxyz";
+  let alphabetArray = alphabetString.split("");
+
+  // Random Alphabet
+  let index = Math.round(Math.random() * 26);
+  return alphabetArray[index];
+}
+
+// Heigh Light Key By id
+function heigLightKeyById(elementId) {
+  let element = document.getElementById(elementId);
+  element.classList.add("bg-orange-500");
+}
